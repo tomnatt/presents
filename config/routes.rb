3af39403315882cafb2e9b1cf719cabed7ac20f0
tomnatt <github@tomnatt.com>
2016-04-01 19:35:51 +0100

    updated homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :presents
 
-  root 'presents#index'
+  root 'welcome#index'
 
   # Authentication by the power of omniauth
   get 'auth/:provider/callback', to: 'sessions#create'
